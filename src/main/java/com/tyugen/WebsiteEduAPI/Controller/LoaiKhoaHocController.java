ee9f89d3c4f92a41f06adbb6aed804f838b57728
datbh <bdat1606@gmail.com>
2023-07-25 07:42:35 +0700

    update an existing loaiKhoaHoc

--- src/main/java/com/tyugen/WebsiteEduAPI/Controller/LoaiKhoaHocController.java
+++ src/main/java/com/tyugen/WebsiteEduAPI/Controller/LoaiKhoaHocController.java
@@ -22,9 +22,9 @@ public class LoaiKhoaHocController {
     private final LoaiKhoaHocRepository loaiKhoaHocRepository;
 
     /**
-     * Constructor
+     * Constructs a new LoaiKhoaHocController object with the specified LoaiKhoaHocRepository.
      *
-     * @param loaiKhoaHocRepository LoaiKhoaHocRepository
+     * @param loaiKhoaHocRepository the LoaiKhoaHocRepository to be used by this controller
      */
     public LoaiKhoaHocController(LoaiKhoaHocRepository loaiKhoaHocRepository) {
         this.loaiKhoaHocRepository = loaiKhoaHocRepository;
@@ -50,5 +50,37 @@ public class LoaiKhoaHocController {
         }
     }
 
+    /**
+     * Updates a LoaiKhoaHoc object with the specified ID.
+     *
+     * @param id          the ID of the LoaiKhoaHoc object to be updated
+     * @param loaiKhoaHoc a JSON representation of the updated LoaiKhoaHoc object
+     * @return a ResponseEntity indicating the result of the update operation
+     */
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateLoaiKhoaHoc(@PathVariable("id") Integer id, @RequestBody String loaiKhoaHoc) {
+        Gson gson = new Gson();
+        LoaiKhoaHoc loaiKhoaHocNew = gson.fromJson(loaiKhoaHoc, LoaiKhoaHoc.class);
+        Optional<LoaiKhoaHoc> loaiKhoaHocOld = loaiKhoaHocRepository.findById(id);
+        if (loaiKhoaHocOld.isPresent()) {
+            Validator validator;
+            try (ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory()) {
+                validator = validatorFactory.getValidator();
+            }
+            Set<ConstraintViolation<LoaiKhoaHoc>> violations = validator.validate(loaiKhoaHocNew);
+            if (violations.size() > 0) {
+                List<String> errorMessages = violations.stream()
+                        .map(ConstraintViolation::getMessage)
+                        .collect(Collectors.toList());
+                return ResponseEntity.badRequest().body(errorMessages);
+            } else {
+                loaiKhoaHocNew.setLoaiKhoaHocID(id);
+                loaiKhoaHocRepository.save(loaiKhoaHocNew);
+                return ResponseEntity.ok().build();
+            }
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
