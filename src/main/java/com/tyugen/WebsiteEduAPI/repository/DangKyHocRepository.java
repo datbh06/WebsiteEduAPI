ab85c06cc11b7bbef7db8abbccb2700b312079f0
datbh06 <bdat1606@gmail.com>
2023-07-26 23:43:47 +0700

    FEAT: update soHocVien

--- src/main/java/com/tyugen/WebsiteEduAPI/repository/DangKyHocRepository.java
+++ src/main/java/com/tyugen/WebsiteEduAPI/repository/DangKyHocRepository.java
@@ -1,7 +1,9 @@
 package com.tyugen.WebsiteEduAPI.repository;
 
 import com.tyugen.WebsiteEduAPI.model.DangKyHoc;
+import com.tyugen.WebsiteEduAPI.model.KhoaHoc;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DangKyHocRepository extends JpaRepository<DangKyHoc, Integer> {
+    int countByKhoaHoc(KhoaHoc khoaHoc);
 }
--- src/main/java/com/tyugen/WebsiteEduAPI/service/KhoaHocService.java
+++ src/main/java/com/tyugen/WebsiteEduAPI/service/KhoaHocService.java
@@ -1,23 +1,33 @@
 package com.tyugen.WebsiteEduAPI.service;
 
 import com.tyugen.WebsiteEduAPI.model.KhoaHoc;
+import com.tyugen.WebsiteEduAPI.repository.DangKyHocRepository;
+import com.tyugen.WebsiteEduAPI.repository.HocVienRepository;
 import com.tyugen.WebsiteEduAPI.repository.KhoaHocRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class KhoaHocService {
     private final KhoaHocRepository khoaHocRepository;
+    private final HocVienRepository hocVienRepository;
+    private final DangKyHocRepository dangKyHocRepository;
 
     /**
      * Constructs a new KhoaHocService object with the specified KhoaHocRepository.
      *
-     * @param khoaHocRepository the khoaHocRepository to be used by this service
+     * @param khoaHocRepository   the khoaHocRepository to be used by this service
+     * @param hocVienRepository   the hocVienRepository to be used by this service
+     * @param dangKyHocRepository the dangKyHocRepository to be used by this service
      */
-    public KhoaHocService(KhoaHocRepository khoaHocRepository) {
+    public KhoaHocService(KhoaHocRepository khoaHocRepository, HocVienRepository hocVienRepository, DangKyHocRepository dangKyHocRepository) {
         this.khoaHocRepository = khoaHocRepository;
+        this.hocVienRepository = hocVienRepository;
+        this.dangKyHocRepository = dangKyHocRepository;
     }
 
     /**
@@ -32,6 +42,16 @@ public class KhoaHocService {
         return khoaHocRepository.findAll(pageable);
     }
 
+    //update soHocVien in KhoaHoc after a new HocVien is added in dangKyHoc
+    public void updateSoHocVien(Integer khoaHocID) {
+        Optional<KhoaHoc> khoaHocOptional = khoaHocRepository.findById(khoaHocID);
+        if (khoaHocOptional.isPresent()) {
+            KhoaHoc khoaHoc = khoaHocOptional.get();
+            int soHocVien = dangKyHocRepository.countByKhoaHoc(khoaHoc);
+            khoaHoc.setSoHocVien(soHocVien);
+            khoaHocRepository.save(khoaHoc);
+        }
+    }
 
 
 }
