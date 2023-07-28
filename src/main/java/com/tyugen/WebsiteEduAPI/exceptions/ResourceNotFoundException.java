2b0225e8cdec47e29c2edbc2709be48d8aa1f099
datbh06 <bdat1606@gmail.com>
2023-07-28 15:59:28 +0700

    docs: java docs for ResourceNotFoundException()

--- src/main/java/com/tyugen/WebsiteEduAPI/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/tyugen/WebsiteEduAPI/exceptions/ResourceNotFoundException.java
@@ -4,6 +4,11 @@ package com.tyugen.WebsiteEduAPI.exceptions;
  * This class represents an exception that is thrown when a resource is not found.
  */
 public class ResourceNotFoundException extends RuntimeException {
+    /**
+     * Constructs a new ResourceNotFoundException object with the specified message.
+     *
+     * @param message the message to be used by this exception
+     */
     public ResourceNotFoundException(String message) {
         super(message);
     }
