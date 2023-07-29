74466bd0d39c6a5711ef37feccd1a7519509a919
datbh06 <bdat1606@gmail.com>
2023-07-29 20:40:42 +0700

    adding method to Find Page of HocVien objects from the database by their hoTen.

--- src/main/java/com/tyugen/WebsiteEduAPI/repository/HocVienRepository.java
+++ src/main/java/com/tyugen/WebsiteEduAPI/repository/HocVienRepository.java
@@ -1,6 +1,8 @@
 package com.tyugen.WebsiteEduAPI.repository;
 
 import com.tyugen.WebsiteEduAPI.model.HocVien;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -34,4 +36,13 @@ public interface HocVienRepository extends JpaRepository<HocVien, Integer> {
      * @return an Optional containing the HocVien object with the specified soDienThoai if it exists
      */
     Optional<HocVien> findBySoDienThoai(String soDienThoai);
+
+    /**
+     * Finds a Page of HocVien objects from the database by their hoTen.
+     *
+     * @param hoTen    the hoTen of the HocVien object to be found
+     * @param pageable the Pageable object used to construct the query
+     * @return a Page containing the HocVien objects with the specified hoTen
+     */
+    Page<HocVien> findByHoTenContaining(String hoTen, Pageable pageable);
 }
