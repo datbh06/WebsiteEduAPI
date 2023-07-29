8a2d8233e2fa8caeb56283f2f5c4f58910e750ca
datbh06 <bdat1606@gmail.com>
2023-07-29 16:13:15 +0700

    docs: adding java docs to KhoaHocRepository

--- src/main/java/com/tyugen/WebsiteEduAPI/repository/KhoaHocRepository.java
+++ src/main/java/com/tyugen/WebsiteEduAPI/repository/KhoaHocRepository.java
@@ -4,6 +4,15 @@ import com.tyugen.WebsiteEduAPI.model.KhoaHoc;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+/**
+ * The KhoaHocRepository interface is used to define custom database methods for the KhoaHoc class.
+ */
 public interface KhoaHocRepository extends JpaRepository<KhoaHoc, Integer>, PagingAndSortingRepository<KhoaHoc, Integer> {
+    /**
+     * Finds a KhoaHoc object from the database by its tenKhoaHoc.
+     *
+     * @param tenKhoaHoc the tenKhoaHoc of the KhoaHoc object to be found
+     * @return an Optional containing the KhoaHoc object with the specified tenKhoaHoc if it exists
+     */
     KhoaHoc findByTenKhoaHoc(String tenKhoaHoc);
 }
