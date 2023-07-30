ea2df176ee662f9cb91f20a2171e9cb498a145ea
datbh <bdat1606@gmail.com>
2023-07-30 21:40:40 +0700

    add new method to check if a TaiKhoan object with specified account name exists

--- src/main/java/com/tyugen/WebsiteEduAPI/repository/TaiKhoanRepository.java
+++ src/main/java/com/tyugen/WebsiteEduAPI/repository/TaiKhoanRepository.java
@@ -3,5 +3,15 @@ package com.tyugen.WebsiteEduAPI.repository;
 import com.tyugen.WebsiteEduAPI.model.TaiKhoan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * TaiKhoanRepository is an interface that extends JpaRepository for class TaiKhoan.
+ */
 public interface TaiKhoanRepository extends JpaRepository<TaiKhoan, Integer> {
+    /**
+     * Checks if a TaiKhoan object with the specified account name exists in the database.
+     *
+     * @param taiKhoan the account name to be checked
+     * @return true if the account name exists, false otherwise
+     */
+    boolean existsByTaiKhoan(String taiKhoan);
 }
