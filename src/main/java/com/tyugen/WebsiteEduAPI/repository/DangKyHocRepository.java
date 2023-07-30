a9e20bceef8909a0f842b4464b9384992dacafe8
datbh06 <bdat1606@gmail.com>
2023-07-30 09:34:56 +0700

    docs: add java docs for countByKhoaHoc

--- src/main/java/com/tyugen/WebsiteEduAPI/repository/DangKyHocRepository.java
+++ src/main/java/com/tyugen/WebsiteEduAPI/repository/DangKyHocRepository.java
@@ -2,8 +2,18 @@ package com.tyugen.WebsiteEduAPI.repository;
 
 import com.tyugen.WebsiteEduAPI.model.DangKyHoc;
 import com.tyugen.WebsiteEduAPI.model.KhoaHoc;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DangKyHocRepository extends JpaRepository<DangKyHoc, Integer> {
+
+    /**
+     * Counts the number of DangKyHoc objects in the database that have the specified KhoaHoc.
+     *
+     * @param khoaHoc the KhoaHoc to be used in the query
+     * @return the number of DangKyHoc objects in the database that have the specified KhoaHoc
+     */
     int countByKhoaHoc(KhoaHoc khoaHoc);
+
 }
